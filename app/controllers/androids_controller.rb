3e9496e4d1c86d4eb774a9608c668c5afdd70539
Rajib Chowdhury <chowdhury284@gmail.com>
2015-04-12 15:42:59 +0530

    tweaks.

--- app/controllers/androids_controller.rb
+++ app/controllers/androids_controller.rb
@@ -3,7 +3,7 @@ class AndroidsController < ApplicationController
   www_zip_file_data = params[:filedata].path
   folder_name = Digest::MD5.hexdigest(Time.now.to_s)
   system("cd ~/appcreator_apps && cordova create #{folder_name}")
-  # system("cd ~/appcreator_apps/#{folder_name} && cordova platform add android && cordova plugin add org.apache.cordova.device && cordova plugin add org.apache.cordova.console")
+  system("cd ~/appcreator_apps/#{folder_name} && cordova platform add android && cordova plugin add org.apache.cordova.device && cordova plugin add org.apache.cordova.console")
   
   #system("rm -rf ~/appcreator_apps/#{folder_name}/www")
   system("cd ~/appcreator_apps/#{folder_name}")
@@ -15,10 +15,10 @@ Zip::ZipFile.open(www_zip_file_data) { |zip_file|
    logger.info f
    logger.info '-----------------------------------'
    FileUtils.mkdir_p(File.dirname(f_path))
-   zip_file.extract(f, f_path) true
+   zip_file.extract(f, f_path) {true}
  }
 }
-  # system("cd ~/appcreator_apps/#{folder_name} && cordova build android")
+  system("cd ~/appcreator_apps/#{folder_name} && cordova build android")
   
   render json: {apk_path: "~/appcreator_apps/#{folder_name}/platforms/android/ant-build/MainActivity-debug.apk"}
  end
