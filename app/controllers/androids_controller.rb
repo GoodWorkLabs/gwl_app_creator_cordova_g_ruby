152a61090744e7a24b4834cb0b7095b54cfa5992
Rajib Chowdhury <chowdhury284@gmail.com>
2015-04-12 15:35:04 +0530

    tweaks.

--- app/controllers/androids_controller.rb
+++ app/controllers/androids_controller.rb
@@ -3,8 +3,10 @@ class AndroidsController < ApplicationController
   www_zip_file_data = params[:filedata].path
   folder_name = Digest::MD5.hexdigest(Time.now.to_s)
   system("cd ~/appcreator_apps && cordova create #{folder_name}")
-  system("cd ~/appcreator_apps/#{folder_name} && cordova platform add android && cordova plugin add org.apache.cordova.device && cordova plugin add org.apache.cordova.console")
+  # system("cd ~/appcreator_apps/#{folder_name} && cordova platform add android && cordova plugin add org.apache.cordova.device && cordova plugin add org.apache.cordova.console")
+  
   #system("rm -rf ~/appcreator_apps/#{folder_name}/www")
+  system("cd ~/appcreator_apps/#{folder_name}")
 Zip::ZipFile.open(www_zip_file_data) { |zip_file|
    zip_file.each { |f|
    f_path=File.join("~/appcreator_apps/#{folder_name}", f.name)
@@ -12,11 +14,11 @@ Zip::ZipFile.open(www_zip_file_data) { |zip_file|
    logger.info '-----------------------------------'
    logger.info f
    logger.info '-----------------------------------'
-   #FileUtils.mkdir_p(File.dirname(f_path))
-   #zip_file.extract(f, f_path)  unless File.exist?(f_path)
+   FileUtils.mkdir_p(File.dirname(f_path))
+   zip_file.extract(f, f_path)  unless File.exist?(f_path)
  }
 }
-  system("cd ~/appcreator_apps/#{folder_name} && cordova build android")
+  # system("cd ~/appcreator_apps/#{folder_name} && cordova build android")
   
   render json: {apk_path: "~/appcreator_apps/#{folder_name}/platforms/android/ant-build/MainActivity-debug.apk"}
  end
