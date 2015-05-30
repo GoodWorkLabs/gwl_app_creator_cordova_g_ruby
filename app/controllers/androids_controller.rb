51811608b089243ce031c322968c16469b187c6e
Rajib Chowdhury <chowdhury284@gmail.com>
2015-05-30 17:40:39 +0530

    added custom name to apps.

--- app/controllers/androids_controller.rb
+++ app/controllers/androids_controller.rb
@@ -1,25 +1,26 @@
 class AndroidsController < ApplicationController
- def create
-  www_zip_file_data = params[:filedata].path
-  folder_name = Digest::MD5.hexdigest(Time.now.to_s)
-  system("cd ~/appcreator_apps && cordova create #{folder_name}")
-  system("cd ~/appcreator_apps/#{folder_name} && cordova platform add android && cordova plugin add org.apache.cordova.device && cordova plugin add org.apache.cordova.console && cordova plugin add https://github.com/phonegap-build/PushPlugin.git")
+  def create
+    www_zip_file_data = params[:filedata].path
+    app_name = params[:appname]
+    folder_name = Digest::MD5.hexdigest(Time.now.to_s)
+    system("cd ~/appcreator_apps && cordova create #{folder_name} com.goodappz.#{app_name} #{app_name}")
+    system("cd ~/appcreator_apps/#{folder_name} && cordova platform add android && cordova plugin add org.apache.cordova.device && cordova plugin add org.apache.cordova.console && cordova plugin add https://github.com/phonegap-build/PushPlugin.git")
   
-  #system("rm -rf ~/appcreator_apps/#{folder_name}/www")
-  system("cd ~/appcreator_apps/#{folder_name}")
-Zip::ZipFile.open(www_zip_file_data) { |zip_file|
-   zip_file.each { |f|
-   f_path=File.join("/home/devops/appcreator_apps/#{folder_name}", f.name)
-   logger.info f_path
-   logger.info '-----------------------------------'
-   logger.info f
-   logger.info '-----------------------------------'
-   FileUtils.mkdir_p(File.dirname(f_path))
-   zip_file.extract(f, f_path) {true}
- }
-}
-  system("cd ~/appcreator_apps/#{folder_name} && cordova build android")
+    #system("rm -rf ~/appcreator_apps/#{folder_name}/www")
+    system("cd ~/appcreator_apps/#{folder_name}")
+    Zip::ZipFile.open(www_zip_file_data) { |zip_file|
+      zip_file.each { |f|
+        f_path=File.join("/home/devops/appcreator_apps/#{folder_name}", f.name)
+        logger.info f_path
+        logger.info '-----------------------------------'
+        logger.info f
+        logger.info '-----------------------------------'
+        FileUtils.mkdir_p(File.dirname(f_path))
+        zip_file.extract(f, f_path) {true}
+      }
+    }
+    system("cd ~/appcreator_apps/#{folder_name} && cordova build android")
   
-  render json: {apk_path: "~/appcreator_apps/#{folder_name}/platforms/android/ant-build/MainActivity-debug.apk"}
- end
+    render json: {apk_path: "~/appcreator_apps/#{folder_name}/platforms/android/ant-build/MainActivity-debug.apk"}
+  end
 end
